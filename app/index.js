573e71efbe620af9be9370a1a5ced27b38f06039
Kapil Neurgaonkar <kapil.n@twinprimelabs.com>
2016-04-14 15:06:08 +0530

    Added callback support on selection of chip-links

--- app/index.js
+++ app/index.js
@@ -1,3 +1,13 @@
 (function(){
     var chipLinksTest = angular.module('ChipLinksClient', ['chip-links']);
+
+    chipLinksTest.config(function($logProvider){
+        $logProvider.debugEnabled(true);
+    });
+
+    chipLinksTest.controller('ChipLinksClientCtrl', function($scope, $log) {
+        $scope.chipLinkSelectHandler = function(chipLinkId) {
+            $log.info("Chip link " + chipLinkId + " has been selected!");
+        };
+    });
 })();
